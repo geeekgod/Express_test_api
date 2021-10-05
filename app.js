ee9c1a7942fcd31118f71d96047dde5c430fcbf5
Rishabh Singh <ris8645208@gmail.com>
2021-10-05 16:14:29 +0530

    feat: add single posts api endpoints

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const posts = require("./data/posts");
-
-// express init
 const app = express();
 
 // port
 const port = process.env.PORT || 3000;
 
 app.listen(port);
+app.use(express.static("public"));
+app.use(express.urlencoded());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.json({
@@ -19,9 +20,16 @@ app.get("/", (req, res) => {
 
 app.get("/users/posts", (req, res) => {
   let sendingPosts = posts;
-  res.send(sendingPosts);
+  res.json(sendingPosts);
 });
 
-app.use(express.static("public"));
-app.use(express.urlencoded());
-app.use(morgan("dev"));
+app.get("/users/posts/:id", (req, res) => {
+  const id = req.params.id;
+  let sendingPosts = posts.find((post) => post.id === parseInt(id));
+  if (sendingPosts) {
+    res.json(sendingPosts);
+  } else {
+    res.status(404);
+    res.json({ error: `Post with id ${id} not found` });
+  }
+});
